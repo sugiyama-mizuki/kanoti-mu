1214188fe418907c1e8032ee96d6132eed0e2aa2
okano-ren <ren20021221@icloud.com>
2022-09-28 10:25:55 +0900

    新規作成

--- my_goal/forms.py
+++ my_goal/forms.py
@@ -4,3 +4,8 @@ class My_GoalCreateForm(forms.ModelForm):
     class Meta:
         model=My_Goal
         fields = ('title', 'content')
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
