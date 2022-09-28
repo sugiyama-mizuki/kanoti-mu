2975952b6825fef560a76d01f4e1290c2416b3f3
sho032 <murakamishoki0323@yahoo.co.jp>
2022-09-28 10:33:43 +0900

    urls.py

--- my_goal/urls.py
+++ my_goal/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'my_goal'
 urlpatterns = [
     path('',views.IndexView.as_view(), name="index"),
+    path('create/',views.My_GoalCreateView.as_view(), name="create"),
 ]
