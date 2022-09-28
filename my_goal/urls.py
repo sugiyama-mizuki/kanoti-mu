6e3ee5f43a836c21b58858ea76837c2284932dc6
sho032 <murakamishoki0323@yahoo.co.jp>
2022-09-28 10:55:43 +0900

    urls.py更新

--- my_goal/urls.py
+++ my_goal/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name = 'my_goal'
 urlpatterns = [
     path('',views.IndexView.as_view(), name="index"),
+    path('my_goal-create/',views.My_GoalCreateView.as_view(), name="create"),
+    path('my_goal-delete/',views.My_GoalDeleteView.as_view(), name="create"),
+    path('my_goal-detail/',views.My_GoaldetailView.as_view(), name="create"),
+    path('my_goal-list/',views.My_GoalListView.as_view(), name="create"),
+    path('my_goal-update/',views.My_GoalUpdateView.as_view(), name="create"),
+
     
 ]
