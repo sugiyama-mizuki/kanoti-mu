ced49bfe3980e1f5d6478432a971735ffbb74fbb
sho032 <murakamishoki0323@yahoo.co.jp>
2022-09-28 11:14:25 +0900

    urlsのname

--- my_goal/urls.py
+++ my_goal/urls.py
@@ -5,10 +5,10 @@ app_name = 'my_goal'
 urlpatterns = [
     path('',views.IndexView.as_view(), name="index"),
     path('my_goal-create/',views.My_GoalCreateView.as_view(), name="create"),
-    path('my_goal-delete/',views.My_GoalDeleteView.as_view(), name="create"),
-    path('my_goal-detail/',views.My_GoaldetailView.as_view(), name="create"),
-    path('my_goal-list/',views.My_GoalListView.as_view(), name="create"),
-    path('my_goal-update/',views.My_GoalUpdateView.as_view(), name="create"),
+    path('my_goal-delete/',views.My_GoalDeleteView.as_view(), name="delete"),
+    path('my_goal-detail/',views.My_GoaldetailView.as_view(), name="detail"),
+    path('my_goal-list/',views.My_GoalListView.as_view(), name="list"),
+    path('my_goal-update/',views.My_GoalUpdateView.as_view(), name="update"),
 
     
 ]
