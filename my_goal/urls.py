0491e6bb336868603fa07deca9c77622b91aff92
sugiyama-mizuki <mk7gac4@gmail.com>
2022-09-28 11:23:55 +0900

    update urls&views

--- my_goal/urls.py
+++ my_goal/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('my_goal-delete/',views.My_GoalDeleteView.as_view(), name="delete"),
     path('my_goal-detail/',views.My_GoaldetailView.as_view(), name="detail"),
     path('my_goal-list/',views.My_GoalListView.as_view(), name="list"),
-    path('my_goal-update/',views.My_GoalUpdateView.as_view(), name="update"),
+    path('my_goal-update/<int:pk>',views.My_GoalUpdateView.as_view(), name="update"),
     
 
     
